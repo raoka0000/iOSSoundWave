323760e12090ea27dc878fbc9a22d26502bb755b
raoka0000 <raoka0000@gmail.com>
2017-07-14 00:59:04 +0900

    Initial Commit

---
+++ 音響メディア論/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  音響メディア論
+//
+//  Created by raoka0000 on 2017/07/14.
+//  Copyright © 2017年 raoka0000. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
